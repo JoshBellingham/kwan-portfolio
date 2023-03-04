1f9d28c73344a00933140f1bbea26f2993abdcf7
Josh Bellingham <92236549+ChimingPork@users.noreply.github.com>
2023-03-03 22:45:32 -0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import "./App.css";
+import Main from "./Components/Main"
+
+export default function App() {
+  return (
+    <div class="flex flex-col h-screen">
+      <div class="shadow-lg shadow-white bg-slate-900">
+        <div class="text-white h-16 mx-auto px-5 flex items-center justify-between">
+          <h1 class="text-2xl hover:text-red-500 transition-colors cursor-default">
+            Kwan Tekasala
+          </h1>
+
+          <ul class="flex items-center gap-5">
+            <li>
+              <a class="hover:text-red-500 transition-colors" href="">
+                Home
+              </a>
+            </li>
+            <li>
+              <a class="hover:text-red-500 transition-colors" href="">
+                Work
+              </a>
+            </li>
+            <li>
+              <a class="hover:text-red-500 transition-colors" href="">
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                class="bg-red-500 p-2 hover:text-red-500 hover:bg-inherit transition-colors"
+                href=""
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <main class="flex-grow">
+      <Main />
+      </main>
+
+      {/*footer*/}
+      <footer class="p-2 bg-slate-900">
+        <div class="text-white h-16 mx-auto px-5 flex items-center justify-center">
+          <ul class="flex items-center gap-5">
+          <li class="text-xl">More Of My Work:</li>
+          <a href="https://kwantekasala6.artstation.com/" target="_blank"><img class="object-contain h-5" src="/src/assets/artstation-logo-white.png"/></a>
+          <a class="inline-flex items-center" href="https://www.youtube.com/@kwantekasala5759" target="_blank"><img class="object-contain h-12" src="/src/assets/youtube-100.png"/>Youtube</a>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  );
+}
---
+++ src/Components/Main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div class="flex items-center justify-center text-white my-12">
+      <img class="my-5" src="./src/assets/avatar.png" />
+      <div class="flex w-1/3 ml-10 space-y-10 flex-col ">
+        <h1 class="text-6xl">Hi! I’m Kwan</h1>
+        <h2 class="text-2xl font-sans">
+          I am a 3D artist based in Vancouver, BC, Canada. I enjoy taking ideas
+          from head and creating them from scratch. I am dedicated to learning
+          and improving my skills. Check out my work!
+        </h2>
+      <a class="text-center shadow-sm shadow-white p-2 w-1/4 bg-red-500 rounded-md" href="#mywork">
+        Check out my work!
+      </a>
+    </div>
+    </div>
+  );
+}
