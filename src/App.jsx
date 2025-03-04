e43adc0dc079c94c644940239b356160266f06f5
Josh Bellingham <92236549+ChimingPork@users.noreply.github.com>
2025-03-04 12:53:53 -0600

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,88 @@
 import "./App.css";
-import Main from "./Components/Main"
+import Main from "./components/Main";
 
 export default function App() {
   return (
-    <div class="flex flex-col h-screen">
-      <div class="shadow-lg shadow-white bg-slate-900">
-        <div class="text-white h-16 mx-auto px-5 flex items-center justify-between">
-          <h1 class="text-2xl hover:text-red-500 transition-colors cursor-default">
-            Kwan Tekasala
-          </h1>
+    <div className="flex flex-col h-screen">
+      <header className="shadow-md bg-slate-900 w-screen">
+        <div className="text-white h-16 mx-auto px-5 flex items-center justify-between">
+          <h1 className="text-2xl cursor-default">Kwan Tekasala</h1>
 
-          <ul class="flex items-center gap-5">
+          <nav aria-label="Main navigation">
+            <ul className="flex items-center gap-5">
+              <li>
+                <a className="hover:text-red-500 transition-colors" href="/">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-red-500 transition-colors"
+                  href="/work"
+                >
+                  Work
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-red-500 transition-colors"
+                  href="/about"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  className="bg-red-500 p-2 active:bg-inherit transition-colors"
+                  href="/contact"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <main className="flex-grow">
+        <Main />
+      </main>
+
+      {/*footer*/}
+      <footer className="p-2 bg-slate-900">
+        <div className="text-white h-16 mx-auto px-5 flex items-center justify-center">
+          <ul className="flex items-center gap-5">
+            <li className="text-xl">More Of My Work:</li>
             <li>
-              <a class="hover:text-red-500 transition-colors" href="">
-                Home
-              </a>
-            </li>
-            <li>
-              <a class="hover:text-red-500 transition-colors" href="">
-                Work
-              </a>
-            </li>
-            <li>
-              <a class="hover:text-red-500 transition-colors" href="">
-                About
+              <a
+                href="https://kwantekasala6.artstation.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Visit my Artstation profile"
+              >
+                <img
+                  className="object-contain h-5"
+                  src="images/artstation-logo-white.png"
+                  alt="Artstation logo"
+                />
               </a>
             </li>
             <li>
               <a
-                class="bg-red-500 p-2 hover:text-red-500 hover:bg-inherit transition-colors"
-                href=""
+                className="inline-flex items-center"
+                href="https://www.youtube.com/@kwantekasala5759"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Visit my YouTube channel"
               >
-                Contact
+                <img
+                  className="object-contain h-12"
+                  src="images/youtube-100.png"
+                  alt="YouTube logo"
+                />
+                <span>Youtube</span>
               </a>
             </li>
-          </ul>
-        </div>
-      </div>
-
-      <main class="flex-grow">
-      <Main />
-      </main>
-
-      {/*footer*/}
-      <footer class="p-2 bg-slate-900">
-        <div class="text-white h-16 mx-auto px-5 flex items-center justify-center">
-          <ul class="flex items-center gap-5">
-          <li class="text-xl">More Of My Work:</li>
-          <a href="https://kwantekasala6.artstation.com/" target="_blank"><img class="object-contain h-5" src="images/artstation-logo-white.png"/></a>
-          <a class="inline-flex items-center" href="https://www.youtube.com/@kwantekasala5759" target="_blank"><img class="object-contain h-12" src="images/youtube-100.png"/>Youtube</a>
           </ul>
         </div>
       </footer>
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,22 +1,43 @@
 import React from "react";
+import Carousel from "./Carousel";
 
 export default function Main() {
   return (
-    <div class="flex items-center justify-center text-white my-12">
-      <img class="my-5" src="images/avatar.png" />
-      <div class="flex w-1/3 ml-10 space-y-10 flex-col ">
-        <h1 class="text-6xl">Hi! I’m Kwan</h1>
-        <h2 class="text-2xl font-sans">
-          I am a 3D artist based in Vancouver, BC, Canada. I enjoy taking ideas
-          from head and creating them from scratch. I am dedicated to learning
-          and improving my skills. Check out my work!
-        </h2>
-        <a
-          class="text-center shadow-sm shadow-white p-2 w-1/4 bg-red-500 rounded-md"
-          href="#mywork"
-        >
-          Check out my work!
-        </a>
+    <div class="flex flex-col">
+      <div class="flex items-center justify-center text-white my-12">
+        <img class="my-5" src="images/avatar.png" />
+        <div class="flex w-1/4 ml-10 space-y-10 flex-col ">
+          <h1 class="text-6xl">Hi! I’m Kwan</h1>
+          <h2 class="text-2xl font-sans">
+            I am a 3D artist based in Vancouver, BC, Canada. I enjoy taking
+            ideas from head and creating them from scratch. I am dedicated to
+            learning and improving my skills. Check out my work!
+          </h2>
+          <a
+            class="active:translate-y-0.5 text-center shadow-sm shadow-white p-2 w-1/3 bg-red-500 rounded-md"
+            href="#mywork"
+          >
+            Check out my work!
+          </a>
+        </div>
+      </div>
+
+      <div class="flex-col items-center justify-center my-5 text-white text-center">
+        <h1 class="text-6xl">My Work</h1>
+      </div>
+
+{/* Shrink Carousels to house less images */}
+
+      <div class="flex justify-between text-center">
+      <div class="2xl:15 2xl:mx-auto 2xl:px-0 py-3 px-10">
+        <Carousel name="Characters"/>
+      </div>
+      <div class="2xl:15 2xl:mx-auto 2xl:px-0 py-3 px-10">
+        <Carousel name="Environments"/>
+      </div>
+      <div class="2xl:15 2xl:mx-auto 2xl:px-0 py-3 px-10">
+        <Carousel name="Assets"/>
+      </div>
       </div>
     </div>
   );
